87da63f06ca63f5e977139d1e3df1e11f06a7a0a
E-ugine <eugineagolla@gmail.com>
2026-08-27 17:01:03 +0300

    Wip wallet_system: Transaction class

--- wallet_system/main.py
+++ wallet_system/main.py
@@ -3,16 +3,16 @@ import datetime
 
 
 class Transaction:
-    def __init__(self, amount, action, ):
+    def __init__(self, amount, transaction_type, ):
         self.id = uuid.uuid4()
         self.amount = amount
-        self.action = action
+        self.transaction_type = transaction_type
         self.timestamp = datetime.datetime.now()
         self.status = "Pending"
 
 
     def __repr__(self):
-        return f"{self.amount} {self.action}"
+        return f"{self.amount} {self.transaction_type}"
 
 
     def __eq__(self,other):
