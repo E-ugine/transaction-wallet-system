2b1dbcc5608ed05aab33d899cefe0338a9131184
E-ugine <agollaeugine@gmail.com>
2026-08-27 20:49:16 +0300

    Update: Transaction class

---
+++ week2_wallet_system/main.py
@@ -0,0 +1,28 @@
+import uuid
+import datetime
+
+
+class Transaction:
+    def __init__(self, amount, transaction_type, ):
+        self.id = uuid.uuid4()
+        self.amount = amount
+        self.transaction_type = transaction_type
+        self.timestamp = datetime.datetime.now()
+        self.status = "Pending"
+
+
+    def __repr__(self):
+        return f"Transaction(id={self.id}, amount={self.amount}, type={self.transaction_type}, status={self.status})"
+
+
+    def __eq__(self,other):
+        if isinstance(other, Transaction):
+            return self.id ==other.id
+        return False
+
+    def __hash__(self):
+        return hash(self.id)
+
+t1 = Transaction(20000, "Deposit")
+t2 = Transaction(20000, "Deposit")
+print(t1 == t2)            
