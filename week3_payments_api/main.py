5a1d54f8f5db376d02d0f23884df5bf4378dfae4
E-ugine <eugineagolla@gmail.com>
2026-09-03 14:33:25 +0300

    Define db models

---
+++ week3_payments_api/main.py
@@ -0,0 +1,27 @@
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column, relationship
+from sqlalchemy import String, DECIMAL, DateTime, ForeignKey
+
+class Base(DeclarativeBase):
+    pass
+
+class Account(Base):
+    __tablename__ = "accounts"
+
+    id : Mapped[int] = mapped_column(primary_key=True)
+    owner : Mapped[str] = mapped_column(String(100))
+    transactions: Mapped[list["Transaction"]] = relationship(back_populates="account")
+    # List tells SQLAlchemy that this is a 1:M relationship. No lists signals M:1/1:1
+
+
+class Transaction(Base):
+    __tablename__ = "transactions"
+
+    id : Mapped[int] = mapped_column(primary_key=True)
+    amount : Mapped[DECIMAL] = mapped_column(DECIMAL(10,2))
+    transaction_type : Mapped[str] = mapped_column(String(100))
+    timestamp: Mapped[DateTime] = mapped_column(DateTime(timezone=True))
+    status : Mapped[str] = mapped_column(String(100))
+
+    account_id : Mapped[int] = mapped_column(ForeignKey("accounts.id"))
+    account: Mapped["Account"] = relationship(back_populates="transactions")
+
